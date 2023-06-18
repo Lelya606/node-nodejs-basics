bc3d700bc1cf66c1fe84a10a4785ca62d4424abf
Lelya606 <elenamironova0302@gmail.ru>
2023-06-18 16:55:17 +0300

    feat: add function copy files

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,24 @@
+import * as fs from "node:fs/promises";
+import { fileURLToPath } from "url";
+import path, { dirname } from "node:path";
+
+const _filename = fileURLToPath(import.meta.url);
+const _dirname = dirname(_filename);
+const sourcePath = path.join(_dirname, "files");
+const newPath = path.join(_dirname, "files_copy");
+
 const copy = async () => {
-    // Write your code here 
+  try {
+    const options =  {
+      recursive: true,
+      force: false,
+      errorOnExist: true,
+    }
+
+    await fs.cp(sourcePath, newPath, options);
+  } catch (error) {
+    throw new Error("FS operation failed");
+  }
 };
 
 await copy();
