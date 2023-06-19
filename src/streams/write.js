67717c948f25b7d2c2b3815d24b775ac8077fd51
Lelya606 <elenamironova0302@gmail.ru>
2023-06-19 09:12:52 +0300

    feat: add function write stream

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,24 @@
+import { createWriteStream } from "fs";
+import { fileURLToPath } from "url";
+import path, { dirname } from "node:path";
+import readline from "node:readline";
+
+const input = process.stdin;
+const output = process.stdout;
+
+const _filename = fileURLToPath(import.meta.url);
+const _dirname = dirname(_filename);
+const filePath = path.join(_dirname, "files", "fileToWrite.txt");
+const prompt = "Type something to see the output:\n";
+const message = '\nGoodbye! Data is saved ("fileToWrite.txt").\n';
+
 const write = async () => {
-    // Write your code here 
+  const writableStream = createWriteStream(filePath);
+  const rl = readline.createInterface({ input, output, prompt });
+
+  rl.prompt();
+  rl.on("line", (input) => writableStream.write(`${input}\n`));
+  rl.on("close", () => output._write(message));
 };
 
 await write();
