5849d3f05d0c26f21a65cdc84a62405ee7b840ca
Lelya606 <elenamironova0302@gmail.ru>
2023-06-19 08:15:09 +0300

    feat: add function parseENV

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,18 @@
+const argv = process.argv;
+
 const parseArgs = () => {
-    // Write your code here 
+  const output = [];
+  const arguments = argv.filter(
+    (element, index, arr) =>
+      element.startsWith("--") || (index > 0 && arr[index - 1].startsWith("--"))
+  );
+
+  arguments.forEach(
+    (element, index, arr) =>
+      element.startsWith("--") && output.push(`${element.slice(2)} is ${arr[index + 1]}`)
+  );
+
+  console.log(output.join(", "));
 };
 
 parseArgs();
