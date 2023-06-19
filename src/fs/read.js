17bbce580be678e4498c8c59f1f75097134a5884
Lelya606 <elenamironova0302@gmail.ru>
2023-06-19 07:52:58 +0300

    feat: add function read

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,18 @@
+import * as fs from "node:fs/promises";
+import { fileURLToPath } from "url";
+import path, { dirname } from "node:path";
+
+const _filename = fileURLToPath(import.meta.url);
+const _dirname = dirname(_filename);
+const filePath = path.join(_dirname, "files", "fileToRead.txt");
+
 const read = async () => {
-    // Write your code here 
+  try {
+    const text = await fs.readFile(filePath, "utf-8");
+    console.log(text);
+  } catch (error) {
+    throw new Error("FS operation failed");
+  }
 };
 
 await read();
